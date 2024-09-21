d617e06d30ce108e0ff9439cf4bce47e2ce0a258
PrajwolM <p.mdr79@gmail.com>
2024-09-21 23:06:42 +1000

    Database fixed

--- healthlogdb (2).sql
+++ healthlogdb (2).sql
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `healthlogdb`
 --
+CREATE DATABASE IF NOT EXISTS `healthlogdb` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `healthlogdb`;
 
 -- --------------------------------------------------------
 
