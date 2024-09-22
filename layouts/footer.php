96e641b5ab064f0792d83c44c8a2ab76adc9fa5d
PrajwolM <p.mdr79@gmail.com>
2024-09-22 15:14:30 +1000

    layout files changed

---
+++ layouts/footer.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Final</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.3.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+</head>
+<body>
+   
+    <footer class="bg-light text-dark py-3">
+        <div class="container">
+            <div class="row py-4">
+                <div class="col-lg-6 col-md-12 mb-4">
+                    <h6 class="mb-3">HealthLogs</h6>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis a consequuntur dolor officiis eos, delectus molestias eveniet accusantium minus quisquam commodi adipisci dicta. Eum ut dignissimos iusto. Assumenda, expedita suscipit.</p>
+                    <div class="social d-flex gap-2 mt-3">
+                        <a href="https://www.facebook.com/" class="text-dark"><i class="fab fa-facebook-f"></i></a>
+                        <a href="https://www.instagram.com/" class="text-dark"><i class="fab fa-instagram"></i></a>
+                        <a href="https://www.twitter.com/" class="text-dark"><i class="fab fa-twitter"></i></a>  
+                    </div>
+                </div>
+                <div class="col-lg-2 col-md-4 mb-4">
+                    <h3>Quick Links</h3>
+                    <ul class="list-unstyled">
+                        <li><a href="#" class="text-dark text-decoration-none">Our Doctors</a></li>
+                        <li><a href="#" class="text-dark text-decoration-none">Services</a></li>
+                        <li><a href="#" class="text-dark text-decoration-none">Packages</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-4 col-md-8 mb-4">
+                    <h3>Contact</h3>
+                    <ul class="list-unstyled">
+                        <li><i class="fas fa-map-marker-alt"></i> <span> 4/255 Elizabeth St, Sydney NSW 2000</span></li>
+                        <li><i class="fas fa-phone"></i> <span> (02) 8236 8037</span></li>
+                        <li><i class="fas fa-envelope"></i> <span> info@healthlogs.com</span></li>
+                    </ul>
+                </div>
+            </div>
+            <hr>
+            <p class="text-center mb-0">Copyright &copy; 2024 HealthLogs. Designed By ShantiPrajwol</p>
+        </div>
+    </footer>
+
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
--- layouts/nav.php
+++ layouts/nav.php
@@ -6,7 +6,7 @@
     <title>Final</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
@@ -18,28 +18,31 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+            <a class="nav-link active" aria-current="page" href="../pages/index.php">Home</a>
             </li>
             <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
+            <a class="nav-link" aria-current="page" href="../pages/services.php">Services</a>
             </li>
-            <li class="nav-item dropdown">
+            <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="../pages/doctor-details.php">Doctors</a>
+            </li>
+            <!-- <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Services
             </a>
             <ul class="dropdown-menu">
                 <li><a class="dropdown-item" href="#">Service Details</a></li>
-                <li><a class="dropdown-item" href="#">Doctor</a></li>
+                <li><a class="dropdown-item" href="../pages/doctor-details.php">Doctor</a></li>
                 <li><a class="dropdown-item" href="#">Appointment</a></li>
-            </ul>
+            </ul> -->
             </li>
             <li class="nav-item">
-            <a class="nav-link" href="#">Contact</a>
+            <a class="nav-link" href="../pages/contact.php">Contact</a>
             </li>
         </ul>
         </div>
     </div>
-    </nav>
+    </nav> 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
